53f571dfd3df1112c86606ea99b10da75cc3b67a
ArshjotSinghh <arshjot.singh@dcmail.ca>
2023-11-22 17:34:00 -0500

    defined all the necessary variables

---
+++ lab4/part1/main.js
@@ -0,0 +1,8 @@
+let customName=document.getElementById('customname');
+let randomize=document.querySelector('.randomize');
+let story = document.querySelector('.story');
+
+let storyText= 'It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+let insertX=['Willy the Goblin','Big Daddy','Father Christmas'];
+let insertY=['the soup kitchen','Disneyland','the White House'];
+let insertZ=['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
