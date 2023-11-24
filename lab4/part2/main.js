e7a501d97b1a78b4df09c9bfabbad9e23915c671
ArshjotSinghh <arshjot.singh@dcmail.ca>
2023-11-24 18:23:53 -0500

    added some javascript logic

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -6,7 +6,10 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-const images=['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg']
+const allImages=['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg']
+
+/* Declaring the alternative text for each image file */
+
 const alts={
     'pic1.jpg':'Closeup of a human eye',
     'pic2.jpg':'Rock that looks like a wave',
@@ -15,14 +18,21 @@ const alts={
     'pic5.jpg':'Large moth on a leaf'
 }
 
-/* Declaring the alternative text for each image file */
-
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for(const image of allImages){
+    const newImage=document.createElement('img');
+    newImage.setAttribute('src',`images/${img}`);
+    thumbBar.appendChild(newImage)
+}
+
+function setEventToEverySingleImage(event){
+    displayedImage.setAttribute('src',event.target.getAttribute('src'));
+}
+
+thumbBar.addEventListener('click',setEventToEverySingleImage)
 
 /* Wiring up the Darken/Lighten button */
+
+
 
